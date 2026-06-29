1096a2c09f1d95b996584336815518d02f419bf6
Matiss <matissciss@gmail.com>
2026-06-29 20:25:00 +0300

    Simplified averageMarkOfStudent

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,28 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         int[][] studentsAndMarks = {{91,75,88},{67,78,87},{98,99,96},{83,76,97},{53,65,55}};
 
-        for(int i = 0 ; i < studentsAndMarks.length; i++) {
-            double studentsAverage = averageMarkOfStudents(studentsAndMarks, i);
-            if(studentsAverage > 95) {
+        for (int i = 0; i < studentsAndMarks.length; i++) {
+            double studentsAverage = averageMarkOfStudents(studentsAndMarks[i]);
+            if (studentsAverage > 95) {
                 System.out.println("Student " + i + " got an A");
             } else if (studentsAverage > 90) {
-                System.out.println("Student " + i + " got an B");
+                System.out.println("Student " + i + " got a B");
             } else if (studentsAverage > 80) {
-                System.out.println("Student " + i + " got an C");
+                System.out.println("Student " + i + " got a C");
             } else if (studentsAverage > 70) {
                 System.out.println("Student " + i + " got an E");
             } else if (studentsAverage < 60) {
                 System.out.println("Student " + i + " got an F and failed the test");
             }
         }
+    }
 
-
-
-
-        }
-    public static double averageMarkOfStudents(int[][] arr, int row) {
+    public static double averageMarkOfStudents(int[] arr) {
         double sum = 0;
-        double averageMark = 0;
-        for(int i = 0; i < arr[row].length; i++) {
-            sum += arr[row][i];
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
         }
-        averageMark = sum / arr[row].length;
-        return averageMark;
+        return sum / arr.length;
     }
 }
